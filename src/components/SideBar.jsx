eccfc8090ae6f0fd9d48ab8bcd3ca0e6dca2b607
Gregory Harris <144550630+GStreet71@users.noreply.github.com>
2024-11-04 07:13:18 -0600

    Limit #root height to 100vh for better scroll handling
    Updated the index.css file to set the height of the #root element to 100vh. This change aims to prevent the occurrence of nested scroll bars when using the SideBar component, ensuring a cleaner and more user-friendly interface.

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -8,16 +8,17 @@ function SideBar(props) {
     <div className='sidebar'>
         <div onClick={ handleToggleModal }
             className="background-overlay"
-        ></div>
-        <div className="sidebar-contents">
-            <h2>{data?.title}</h2>
-            <div className='description-container'>
-                <p className="description-title">{data?.date}</p>
-                <p>{data?.explanation}</p>
+        >
+            <div className="sidebar-contents">
+                <h2>{data?.title}</h2>
+                <div className='description-container'>
+                    <p className="description-title">{data?.date}</p>
+                    <p>{data?.explanation}</p>
+                </div>
+                <button onClick={ handleToggleModal }>
+                    <i className="fa-solid fa-arrow-right" />
+                </button>
             </div>
-            <button onClick={ handleToggleModal }>
-                <i className="fa-solid fa-arrow-right" />
-            </button>
         </div>
     </div>
   )
