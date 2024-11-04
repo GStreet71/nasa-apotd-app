f6e696e4d1b382a159287fbc4ea3dea84ed994bd
Gregory Harris <144550630+GStreet71@users.noreply.github.com>
2024-11-04 09:23:30 -0600

    Update SideBar.jsx
    Updated sidebar background from transparent to black on mobile devices.

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -7,19 +7,17 @@ function SideBar(props) {
   return (
     <div className='sidebar'>
         <div onClick={ handleToggleModal }
-            className="background-overlay"
-        >
-            <div className="sidebar-contents">
-                <h2>{data?.title}</h2>
-                <div className='description-container'>
-                    <p className="description-title">{data?.date}</p>
-                    <p>{data?.explanation}</p>
-                </div>
-                <button onClick={ handleToggleModal }>
-                    <i className="fa-solid fa-arrow-right" />
-                </button>
+            className="background-overlay"></div>
+        <div className="sidebar-contents">
+            <h2>{data?.title}</h2>
+            <div className='description-container'>
+                <p className="description-title">{data?.date}</p>
+                <p>{data?.explanation}</p>
             </div>
-        </div>
+            <button onClick={ handleToggleModal }>
+                <i className="fa-solid fa-arrow-right" />
+            </button>
+        </div>        
     </div>
   )
 }
